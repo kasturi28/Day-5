ae9a5ce80ca9ab27814642713124af5f68956c79
kasturi28 <kasturipansare2818@gmail.com>
2022-06-24 23:10:01 +0530

    FlipCoin.java

---
+++ src/FlipCoin.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class FlipCoin {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * 
+		 */
+		Random random = new Random();
+        float x1 = random.nextFloat(1);
+
+        if (x1 < 0.5) {
+            System.out.println("COIN DISPLAYS HEAD....");
+        } else {
+            System.out.println("COIN DISPLAYS TAIL....");
+        }
+
+	}
+
+}
